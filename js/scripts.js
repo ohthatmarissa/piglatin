560440fb3ed2a00968a85432576780b03643b7db
Marissa Perry and Zach Weintraub <marissa+zach@>
2019-04-03 13:37:32 -0700

    add functionality to move multiple consonants from the beginning, as well as capture qu

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,26 @@
 function translate(string) {
   var inputArray = string.split('');
-  var vowels = ["a", "e", "i", "o", "u"];
-  if(!vowels.includes(inputArray[0])) {
-    inputArray.push(inputArray.shift());
+  var vowels = ['a', 'e', 'i', 'o', 'u'];
+  var consonantCluster = ['-'];
+  while(!vowels.includes(inputArray[0])) {
+    if(inputArray[0] === 'q' && inputArray[1] === 'u') {
+      consonantCluster.push(inputArray.shift());
+      consonantCluster.push(inputArray.shift());
+    } else {
+      consonantCluster.push(inputArray.shift());
+    }
   }
-  string = inputArray.join('');
-  string = string.concat("-ay");
+  string = inputArray.join('') + consonantCluster.join('');
+  string = string.concat('ay');
   return string;
-};
+}
 
 
 $().ready(function() {
-  $("form#pigLatin").submit(function(event) {
+  $('form#pigLatin').submit(function(event) {
     event.preventDefault();
-    var userInput = $("#string").val();
+    var userInput = $('#string').val();
     var result = translate(userInput);
-    $("#result p").text(result);
+    $('#result p').text(result);
   })
 });
