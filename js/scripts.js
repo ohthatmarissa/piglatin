58924b642e4b920ad1ab3577fa721b9434b36546
Marissa Perry and Zach Weintraub <marissa+zach@>
2019-04-03 11:02:05 -0700

    add functionality to determine whether first char is consonant and move it if so

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
 function translate(string) {
+  var inputArray = string.split('');
+  var vowels = ["a", "e", "i", "o", "u"];
+  if(!vowels.includes(inputArray[0])) {
+    inputArray.push(inputArray.shift());
+  }
+  string = inputArray.join('');
   string = string.concat("-ay");
   return string;
 };
